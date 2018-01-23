8e84d6b4def46107d0b6178152d2a0e6db5cc351
jsl <1225339091@qq.com>
2018-01-23 10:57:01 +0800

    Signed-off-by: jsl <1225339091@qq.com>

--- src/com/gkzy/mng/bean/MenuOne.java
+++ src/com/gkzy/mng/bean/MenuOne.java
@@ -37,88 +37,96 @@ public class MenuOne {
 	private Integer gdp005;
 	private String gdp006;
 	
-	private List<Privilegemng> privilegemngList = new ArrayList<Privilegemng>();
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name = "menuone_id",unique = true,nullable = false)
-	private Integer getMenuOneId() {
+	public Integer getMenuOneId() {
 		return menuOneId;
 	}
-	private void setMenuOneId(Integer menuOneId) {
+	public void setMenuOneId(Integer menuOneId) {
 		this.menuOneId = menuOneId;
 	}
-	
 	@Column(name = "menu_name",nullable = false)
-	private String getMenuName() {
+	public String getMenuName() {
 		return menuName;
 	}
-	private void setMenuName(String menuName) {
+	public void setMenuName(String menuName) {
 		this.menuName = menuName;
 	}
 	@Column(name = "menu_url")
-	private String getMenuUrl() {
+	public String getMenuUrl() {
 		return menuUrl;
 	}
-	private void setMenuUrl(String menuUrl) {
+	public void setMenuUrl(String menuUrl) {
 		this.menuUrl = menuUrl;
 	}
 	@Column(name = "menu_num",nullable = false)
-	private Integer getMenuNum() {
+	public Integer getMenuNum() {
 		return menuNum;
 	}
-	private void setMenuNum(Integer menuNum) {
+	public void setMenuNum(Integer menuNum) {
 		this.menuNum = menuNum;
 	}
 
 	@Column(name="gdp001")
-	private String getGdp001() {
+	public String getGdp001() {
 		return gdp001;
 	}
-	private void setGdp001(String gdp001) {
+	public void setGdp001(String gdp001) {
 		this.gdp001 = gdp001;
 	}
 	
 	@Column(name="gdp002")
-	private Date getGdp002() {
+	public Date getGdp002() {
 		return gdp002;
 	}
-	private void setGdp002(Date gdp002) {
+	public void setGdp002(Date gdp002) {
 		this.gdp002 = gdp002;
 	}
 	
 	@Column(name="gdp003")
-	private String getGdp003() {
+	public String getGdp003() {
 		return gdp003;
 	}
-	
-	private void setGdp003(String gdp003) {
+	public void setGdp003(String gdp003) {
 		this.gdp003 = gdp003;
 	}
 	
 	@Column(name="gdp004")
-	private Date getGdp004() {
+	public Date getGdp004() {
 		return gdp004;
 	}
-	private void setGdp004(Date gdp004) {
+	public void setGdp004(Date gdp004) {
 		this.gdp004 = gdp004;
 	}
 	
 	@Column(name="gdp005")
-	private Integer getGdp005() {
+	public Integer getGdp005() {
 		return gdp005;
 	}
-	private void setGdp005(Integer gdp005) {
+	public void setGdp005(Integer gdp005) {
 		this.gdp005 = gdp005;
 	}
-	
+
 	@Column(name="gdp006")
-	private String getGdp006() {
+	public String getGdp006() {
 		return gdp006;
 	}
-	private void setGdp006(String gdp006) {
+	public void setGdp006(String gdp006) {
 		this.gdp006 = gdp006;
 	}
+	
+	private List<Privilegemng> privilegemngList = new ArrayList<Privilegemng>();
+	
+
+	
+	
+	
+	
+	
+	
+	
 	
 	@ManyToMany(targetEntity=Privilegemng.class,cascade=CascadeType.ALL,fetch=FetchType.LAZY)
     @JoinTable(name = "menuone_privilegemng",
